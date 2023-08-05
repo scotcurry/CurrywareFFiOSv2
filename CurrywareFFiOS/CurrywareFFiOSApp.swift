c63989d2c86a7b400fdde93ae4f8092a265a660b
Scot Curry <scotcurry4@gmail.com>
2023-08-05 09:44:43 -0400

    Initial Commit

---
+++ CurrywareFFiOS/CurrywareFFiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrywareFFiOSApp.swift
+//  CurrywareFFiOS
+//
+//  Created by Scot Curry on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrywareFFiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
